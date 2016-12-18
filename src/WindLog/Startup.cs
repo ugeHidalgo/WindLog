cbb0be0fb6800a359d59e3d4bf077ad6464fc33c
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2016-12-18 19:19:49 +0100

    Initial project structure with MVC support

--- src/WindLog/Startup.cs
+++ src/WindLog/Startup.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WindLog
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+
+        public Startup( IHostingEnvironment env)
+        {
+            _env = env;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(config =>
+            {
+                if (_env.IsProduction())
+                {
+                    config.Filters.Add(new RequireHttpsAttribute());
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +37,17 @@ namespace WindLog
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            //Para que haga uso de MVC y así valla a index.cshtml
+
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" } //Página de inicio por defecto
+                    );
             });
         }
     }
