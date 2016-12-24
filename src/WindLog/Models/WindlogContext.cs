69ea2cd99fa07acb7a9e582cad501203fb9268ee
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2016-12-24 17:28:15 +0100

    Added DB context class

---
+++ src/WindLog/Models/WindlogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WindLog.Models
+{
+    public class WindlogContext : DbContext
+    {        
+        public DbSet<Spot> Spots { get; set; }        
+        public DbSet<MaterialType> MaterialTypes { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+
+
+        public WindlogContext()
+        {
+
+        }        
+    }
+}
