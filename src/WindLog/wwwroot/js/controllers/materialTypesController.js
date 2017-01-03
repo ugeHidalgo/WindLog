4172c931c180663f130409a860f39752ef5077a5
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2017-01-03 17:07:52 +0100

    Add angular and use on materialTypes screen with mocked data.

---
+++ src/WindLog/wwwroot/js/controllers/materialTypesController.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app-materialTypes')
+        .controller('materialTypesController', materialTypesController);
+
+    function materialTypesController() {
+        var vm = this;
+        vm.materialTypes = [{
+            created: new Date(),
+            name: "Titan",
+            brand: "Naish",
+            model: "Titan 110"
+        }, {
+            created: new Date(),
+            name: "Rocket",
+            brand: "Tabou",
+            model: "Rocket 95"
+        }, {
+            created: new Date(),
+            name: "RRD Wave",
+            brand: "RRD",
+            model: "Wave Cult 82 LTD"
+        }];
+    }
+})();
