91a034841038007e5ddaa7b0e7ddc9de7eab6b2d
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2017-01-03 18:17:21 +0100

    Use angular routing on MaterialTypes.

--- src/WindLog/wwwroot/js/controllers/app-materialTypes.js
+++ src/WindLog/wwwroot/js/controllers/app-materialTypes.js
@@ -1,3 +1,14 @@
 ﻿(function () {
-    angular.module('app-materialTypes', ['simpleControls']);
+    angular.module('app-materialTypes', ['simpleControls','ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'materialTypesController',
+            controllerAs: 'vm',
+            templateUrl: '/views/materialTypesView.html'
+        });
+
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    });
 })();
