4e72d40c37b636b53a1283f40358e9858a90a45b
Eugenio Hidalgo <ugeHidalgo@hotmail.com>
2017-01-15 17:34:39 +0100

    Added new form to edit materials. Clear and new actions added to controller.

--- src/WindLog/wwwroot/js/controllers/material/materialsEditorController.js
+++ src/WindLog/wwwroot/js/controllers/material/materialsEditorController.js
@@ -12,7 +12,28 @@
         vm.isBusy = true;        
         vm.material = {
             id: vm.id,
-            name: 'Una tabla'
-        };        
+            name: 'Rocket95',
+            brand: 'Tabou',
+            model: 'Rocket 95',
+            year: '2010',
+            datePurchased: new Date(2016, 4, 2),
+            dateCreated: new Date(),
+            memo: 'Comprada a SpinOut Tarifa.',
+            materialType: {
+                id: '9',
+                name: 'Board'
+            }
+        };
+
+        vm.newItem = function () {
+            vm.material = {};
+            vm.material.id = 0;
+            vm.material.dateCreated = new Date();
+            vm.material.datePurchased = new Date();
+        };
+
+        vm.clearSelectedItem = function () {
+            vm.material = {};
+        };
     }
 })();
